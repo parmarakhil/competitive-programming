865a28559917b45bb8a87df9c6fb9c0d4eb9385b
Akhil Parmar <akhilparmar2496@gmail.com>
2022-03-10 10:48:10 +0530

    add MySqrt

---
+++ src/MySqrt.java
@@ -0,0 +1,13 @@
+public class MySqrt {
+    public int mySqrt(int x) {
+        if (x <= 1) return x;
+        int left = 0;
+        int right = x;
+        while(right - left > 1){
+            int mid = (left + right) / 2;
+            if (x / mid < mid) right = mid;
+            else left = mid;
+        }
+        return left;
+    }
+}
