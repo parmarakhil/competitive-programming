04892b24f681bba1c35b8fb4bad2e1677475eb5a
Akhil Parmar <akhilparmar2496@gmail.com>
2022-07-26 23:07:33 +0530

    add InsertionSortList

---
+++ src/InsertionSortList.java
@@ -0,0 +1,22 @@
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(-1), curr = head;
+        while(curr!=null){
+            ListNode temp = curr.next;
+            ListNode prev = dummy;
+            ListNode nxt = dummy.next;
+
+            while(nxt!=null){
+                if(nxt.val>curr.val)
+                    break;
+                prev = nxt;
+                nxt = nxt.next;
+            }
+
+            curr.next = nxt;
+            prev.next = curr;
+            curr = temp;
+        }
+        return dummy.next;
+    }
+}
