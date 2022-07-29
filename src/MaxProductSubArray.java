32db1e1c6b42f535738cbb023b486736d543c52f
Akhil Parmar <akhilparmar2496@gmail.com>
2022-07-29 16:48:30 +0530

    add MaxProductSubArray

---
+++ src/MaxProductSubArray.java
@@ -0,0 +1,24 @@
+public class MaxProductSubArray {
+
+
+    public int maxProduct(int[] nums) {
+        int res = Integer.MIN_VALUE;
+        int prd =1;
+        for(int i =0;i<nums.length;i++){
+            prd = prd*nums[i];
+            res = Math.max(res , prd);
+            if(prd==0){
+                prd =1;
+            }
+        }
+        prd = 1;
+        for(int i =nums.length-1;i>=0;i--){
+            prd  = prd*nums[i];
+            res =Math.max(res, prd);
+            if(prd==0){
+                prd =1;
+            }
+        }
+        return res;
+    }
+}
