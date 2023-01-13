872e54704975226d21084a1bcefeff205b3f5e53
Akhil Parmar <akhilparmar2496@gmail.com>
2023-01-13 20:10:55 +0530

    add PopulateNextRightPointer

---
+++ src/PopulateNextRightPointer.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PopulateNextRightPointer {
+
+    public Node connect(Node root) {
+        if(root == null) return null;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            Node rightNode = null;
+            for(int i = q.size(); i > 0; i--) {
+                Node cur = q.poll();
+                cur.next = rightNode;
+                rightNode = cur;
+                if(cur.right != null) {
+                    q.offer(cur.right);
+                    q.offer(cur.left);
+                }
+            }
+        }
+        return root;
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
