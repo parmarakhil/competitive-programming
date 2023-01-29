66359547283d92dcdcc7c26eb3952d6722f79f1d
Akhil Parmar <akhilparmar2496@gmail.com>
2023-01-29 21:10:49 +0530

    add TrailingZeroes

---
+++ src/TrailingZeroes.java
@@ -0,0 +1,12 @@
+public class TrailingZeroes {
+        public int trailingZeroes(int n) {
+            int ans = 0;
+            for (int i = 5; i <= n; i *= 5) {
+                ans += n / i;
+            }
+
+            return ans;
+        }
+
+// TC: O(log5(n)), SC: O(1)
+}
