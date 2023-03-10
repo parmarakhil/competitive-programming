30cd446167728892ff186eff0d9c07bf5e05b5c9
Akhil Parmar <akhilparmar2496@gmail.com>
2023-03-10 17:20:27 +0530

    add CombinationSum3

---
+++ src/CombinationSum3.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum3 {
+
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> currCombination = new ArrayList<>();
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        makeCombination(k,n,1);
+        return ans;
+    }
+    public void makeCombination(int k,int n,int start)
+    {
+        if(k==0 && n==0)
+        {
+            ans.add(new ArrayList<>(currCombination));
+            return;
+        }
+        for(int i=start;i<10;++i)
+        {
+            currCombination.add(i);
+            makeCombination(k-1,n-i,i+1);
+            currCombination.remove(currCombination.size()-1);
+        }
+    }
+}
