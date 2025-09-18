83ea5bf664245b3d74acf066585a4cbcf75f0d7d
parmarakhil <akhilparmar2496@gmail.com>
2025-09-18 12:46:51 +0530

    add AddTwoNumberInLinkedList

--- src/AddTwoNumberInLinkedList.java
+++ src/AddTwoNumberInLinkedList.java
@@ -61,4 +61,111 @@ public class AddTwoNumberInLinkedList {
         }
         return result;
     }
+
+
+    public static ListNode reverse(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        while (current!=null) {
+            ListNode front = current.next;
+            current.next = prev;
+            prev = current;
+            current = front;
+        }
+        return prev;
+    }
+
+    static void print(ListNode head) {
+        ListNode temp= head;
+        while (temp!=null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(9);
+        head.next = new ListNode(9);
+        head.next.next = new ListNode(9);
+        head.next.next.next = new ListNode(9);
+
+
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(4);
+        ListNode answer = addTwoNumbers1(head, head1);
+        print(answer);
+    }
+
+    public static ListNode addTwoNumbers(ListNode head1, ListNode head2) {
+        head1 = reverse(head1);
+        head2 = reverse(head2);
+        ListNode newHead = addNumbers(head1, head2);
+        return reverse(newHead);
+    }
+
+    private static ListNode addNumbers(ListNode head1, ListNode head2) {
+        ListNode head = new ListNode();
+        ListNode current = head;
+        int carry = 0;
+        while (head1!=null || head2!=null) {
+            int number = carry;
+            if (head1!=null) {
+                number+=head1.val;
+                head1 = head1.next;
+            }
+            if (head2!=null) {
+                number+=head2.val;
+                head2 = head2.next;
+            }
+            carry = number/10;
+            head.next = new ListNode(number%10);
+            head = head.next;
+        }
+        if (carry!=0) {
+            head.next = new ListNode(carry);
+            head = head.next;
+        }
+        return current.next;
+
+    }
+
+    public static ListNode addTwoNumbers1(ListNode head1, ListNode head2) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        while (head1!= null || head2!=null) {
+            if (head1!=null) {
+                stack1.push(head1.val);
+                head1 = head1.next;
+            }
+            if (head2!=null) {
+                stack2.push(head2.val);
+                head2 = head2.next;
+            }
+        }
+        int carry =0;
+        ListNode head = new ListNode();;
+        ListNode current = head;
+        while (!stack1.isEmpty() || !stack2.isEmpty()) {
+            int number = carry;
+            if (!stack1.isEmpty()) {
+                number+=stack1.pop();
+            }
+            if (!stack2.isEmpty()) {
+                number+=stack2.pop();
+            }
+            carry = number/10;
+            number = number%10;
+            current.next= new ListNode(number);
+            current = current.next;
+        }
+        if (carry != 0) {
+            current.next= new ListNode(carry);
+            current = current.next;
+        }
+        return reverse(head.next);
+    }
 }
