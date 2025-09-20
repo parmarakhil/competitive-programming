98e83b1a0c5ff62910f09ec409772ad85d603fac
parmarakhil <akhilparmar2496@gmail.com>
2025-09-20 11:02:24 +0530

    add AddTwoNuumbers

--- src/AddTwoNuumbers.java
+++ src/AddTwoNuumbers.java
@@ -26,7 +26,7 @@ public class AddTwoNuumbers {
 }
 class ListNode {
       int val;
-     ListNode next;
+      ListNode next;
       ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
