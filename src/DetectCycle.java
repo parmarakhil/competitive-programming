c7c1f7c9a26bf4a4c6b4c7b7ea16ccfa6789960f
parmarakhil <akhilparmar2496@gmail.com>
2025-10-14 20:50:09 +0530

    add DetectCycle

--- src/DetectCycle.java
+++ src/DetectCycle.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class DetectCycle {
     public ListNode detectCycle(ListNode head) {
         if(head==null || head.next==null) return null;
@@ -17,6 +19,40 @@ public class DetectCycle {
             slow=slow.next;
         }
         return fast;
+    }
+
+
+    public ListNode detectCycle1(ListNode head) {
+        HashSet<ListNode> visited = new HashSet<>();
+        ListNode temp = head;
+        ListNode looped = null;
+        while (temp!=null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            visited.add(temp);
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public ListNode detectCycle2(ListNode head) {
+        if (head == null || head.next==null) return null;
+        ListNode fast = head.next.next;
+        ListNode slow = head.next;
+        // identify loop
+        while (fast!=slow) {
+            if (fast==null || fast.next==null) return null;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        // now we know loop exists so find the now. Node will be equal distance from head and slow/fast
+        slow = head;
+        while (slow!=fast) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return null;
     }
     class ListNode {
       int val;
